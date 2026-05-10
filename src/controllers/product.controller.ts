63d6abd48725bc176d9e0e9b200f89dfdfcea653
tizul-islam <tizulislamtt@gmail.com>
2026-05-10 23:41:41 +0600

    feat: implement product service, controller, and JSON database for fetching product data

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,28 +1,25 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
+import { readProduct } from "../service/product.service.ts";
 
 export const productController = (req: IncomingMessage, res: ServerResponse) => {
     const url = req.url;
     const method = req.method;
 
-
-
-
-
     if (url?.startsWith("/products") && method === "GET") {
 
+        // const products =[
 
-        const products =[
+        //     {
 
-            {
-
-                id:1,
-                name:"Product 1",
-                price:100
-            }
-        ] 
+        //         id:1,
+        //         name:"Product 1",
+        //         price:100
+        //     }
+        // ];
+        const products = readProduct();
 
         res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ message: "this is products router ",data:products }))
+        res.end(JSON.stringify({ message: "this is products router ", data: products }))
     }
 
 }  
---
+++ src/service/product.service.ts
@@ -0,0 +1,23 @@
+import * as path from "node:path";
+import * as fs from "node:fs";
+
+const filePath = path.join(process.cwd(), "src", "database", "data.json");
+
+
+export const readProduct = () => {
+
+    // console.log(filePath);
+
+    const products = fs.readFileSync(filePath, 'utf-8');
+
+    console.log(JSON.parse(products));
+    return JSON.parse(products);
+
+
+
+
+
+
+
+
+}
